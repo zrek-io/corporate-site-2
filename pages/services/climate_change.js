a0178e7832d047ed62fb27a9c05cd41c16375738
Zrek <93639200+zrek-co@users.noreply.github.com>
2021-11-19 12:18:00 +0900

    クリエイター支援画像変更

--- pages/services/climate_change.js
+++ pages/services/climate_change.js
@@ -27,7 +27,7 @@ export default function ClimateChange() {
       <Box as="main" w="80%" mx="auto">
         <Box display="flex" alignItems="center" justifyContent="center" my={20}>
           <Box w={{ sm: "100%", lg: "50%" }}>
-            <SectionTitle SectionTitle="クリエイターのインフラ構築" />
+            <SectionTitle SectionTitle="クリエイターのインフラ構築"/>
             <Text>
               私たちは、デジタルテクノロジーを活用して世界中のクリエイターの生活に欠かせないサービスを提供していきます。大好きなクリエイターがいる方、推しがいる方、創ることが大好きなクリエイターの皆様、ぜひ一度お話をしたいです。困っていることや悩みごとを聞かせてください。お気軽にご連絡ください。今村 → y.imamura@z-rek.com
             </Text>
@@ -41,14 +41,16 @@ export default function ClimateChange() {
             />
           </Box>
         </Box>
-//         <Box w="100%">
-//           <Image
-//             src={HomeServiceImage.src}
-//             w={{ sm: "90%", md: "75%", lg: "45%" }}
-//             mx="auto"
-//             alt="Zrekの提供サービス"
-//           />
-//         </Box>
+/*
+        <Box w="100%">
+          <Image
+            src={HomeServiceImage.src}
+            w={{ sm: "90%", md: "75%", lg: "45%" }}
+            mx="auto"
+            alt="Zrekの提供サービス"
+          />
+        </Box>
+*/
         <Box p={{ sm: 6, lg: 12 }} textAlign="center">
           <PrimaryButton
             title="問い合わせ"
@@ -56,11 +58,13 @@ export default function ClimateChange() {
             margin={{ sm: "4", lg: "8" }}
             onClick={onClickContact}
           />
+/*              
           <BorderButton
             title="もっと詳しく"
             width={{ sm: "65%", md: "30%", lg: "17%" }}
             onClick={onClickMore}
           />
+*/          
         </Box>
       </Box>
       <Footer />
