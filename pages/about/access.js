c3196c728615f3eda82b4c37f413c1e8c37507b6
Yuki Imamura <59524938+yuk6ra@users.noreply.github.com>
2022-06-19 08:55:51 +0900

    Update access.js

--- pages/about/access.js
+++ pages/about/access.js
@@ -14,18 +14,17 @@ export default function Access() {
       <Box as="main" display="flex" flexWrap="wrap" justifyContent="space-between" w={{ sm: '90%', md: '80%', lg: "60%" }} mx="auto" my={{ sm: 16, lg: 32 }}>
         <Box w="100%" mb={20}>
           <SectionTitle SectionTitle="所在地" />
-          <Text>〒248-0007 神奈川県鎌倉市大町1丁目1-14 HATSU鎌倉</Text>
+          <Text>〒247-0056 神奈川県鎌倉市大船１丁目１１－２０鎌倉石原ビル５Ｆ</Text>
         </Box>
         <Box w="100%" mb={20}>
           <SectionTitle SectionTitle="交通手段" />
-          <Text>JR鎌倉駅東口 徒歩5分</Text>
+          <Text>JR大船駅 徒歩3分</Text>
         </Box>
         <Box w="100%" mb={2}>
           <SectionTitle SectionTitle="アクセス" />
         </Box>
         <Box w={{ sm: '90%', lg: "80%" }} h={{ sm: '25vh', md: '40vh', lg: "65vh" }} mx="auto">
-          <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13022.420821146547!2d139.5512588!3d35.31579!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xeaa39e41935d0a9d!2zSEFUU1XpjozlgIk!5e0!3m2!1sja!2sjp!4v1635839508625!5m2!1sja!2sjp" width="100%" height="100%" allowfullscreen="" loading="lazy"></iframe>
-        </Box>
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3254.1525647776944!2d139.5316787!3d35.3518599!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60184560950297fd%3A0x80cb30232f0f967a!2z44CSMjQ3LTAwNTYg56We5aWI5bed55yM6Y6M5YCJ5biC5aSn6Ii577yR5LiB55uu77yR77yR4oiS77yS77yQIOmOjOWAieefs-WOn-ODk-ODqyA1Zg!5e0!3m2!1sja!2sjp!4v1655596501389!5m2!1sja!2sjp" width="100%" height="100%" allowfullscreen="" loading="lazy"></iframe>
       </Box>
       <Footer />
     </Box>
