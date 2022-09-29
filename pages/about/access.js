9b049640988422bc2655d1df79ff261e0c071b4b
Zrek <93639200+zrek-co@users.noreply.github.com>
2022-09-29 11:03:33 +0900

    Update access.js

--- pages/about/access.js
+++ pages/about/access.js
@@ -25,8 +25,9 @@ export default function Access() {
         </Box>
         <Box w={{ sm: '90%', lg: "80%" }} h={{ sm: '25vh', md: '40vh', lg: "65vh" }} mx="auto">
           <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3254.1525647776944!2d139.5316787!3d35.3518599!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60184560950297fd%3A0x80cb30232f0f967a!2z44CSMjQ3LTAwNTYg56We5aWI5bed55yM6Y6M5YCJ5biC5aSn6Ii577yR5LiB55uu77yR77yR4oiS77yS77yQIOmOjOWAieefs-WOn-ODk-ODqyA1Zg!5e0!3m2!1sja!2sjp!4v1655596501389!5m2!1sja!2sjp" width="100%" height="100%" allowfullscreen="" loading="lazy"></iframe>
+        </Box>
       </Box>
       <Footer />
     </Box>
   )
-}
+}   
