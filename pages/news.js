5d4723e295a5497fc0352fe15ae91b6cf1c3bd04
Zrek <93639200+zrek-co@users.noreply.github.com>
2022-09-29 12:00:51 +0900

    Update news.js

--- pages/news.js
+++ pages/news.js
@@ -12,13 +12,13 @@ export default function News() {
       <Header />
       <Hero title="ニュース" />
       <Box as="main" display="flex" flexWrap="wrap" justifyContent="space-between" w={{ sm: '90%', md: '80%', lg: "73%" }} mx="auto" my={32}>
-        <NewsItem date="2021.11.9" tag="コーポレート" title="クリエイター支援事業を開始しました" />
+        <NewsItem date="2022.7.1" tag="サービス" title="NFT事業を開始しました" />
+        <NewsItem date="2022.3.28" tag="コーポレート" title="本店を移転しました" />
         <NewsItem date="2021.11.3" tag="お知らせ" title="コーポレートサイトをリニューアルしました" />
         <NewsItem date="2021.9.18" tag="お知らせ" title="代表の今村がHATSUFESで成果発表会を行いました" />
-        <NewsItem date="2021.8.23" tag="サービス" title="カーボンカードのランディングページを公開しました" />
         <NewsItem date="2021.7.3" tag="サービス" title="地域コミュニティサービス『vivicom』の実証実験を開始しました" />
         <NewsItem date="2021.7.2" tag="コーポレート" title=" Impact Hub APACとバンク・オブ・アメリカが提供するAccelerate Membership Programに採択されました" />
-        <NewsItem date="2021.6.13" tag="コーポレート" title="鈴木隆道氏が顧問に就任しました" />
+        <NewsItem date="2021.6.13" tag="コーポレート" title="元東京都議会議員 鈴木隆道氏が顧問に就任しました" />
         <NewsItem date="2021.6.9" tag="お知らせ" title="コーポレートサイトをリニューアルしました" />
         <NewsItem date="2021.5.1" tag="お知らせ" title="神奈川県庁の起業支援プログラムに代表の今村が採択されました" />
         <NewsItem date="2021.4.1" tag="コーポレート" title="株式会社Zrek（本社：神奈川県鎌倉市）を設立しました" />
